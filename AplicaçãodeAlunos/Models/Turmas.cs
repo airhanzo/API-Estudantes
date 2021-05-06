6aed8ff11e0092c63c5d0069ef60275f0bcd1c58
airhanzo <opedrohanzo@gmail.com>
2021-05-05 23:21:20 -0300

    Adicionar arquivos de projeto.

---
+++ AplicaçãodeAlunos/Models/Turmas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicaçãodeAlunos.Models
+{
+    public class Turmas
+    {
+        public Turmas (int id)
+        {
+            Id = id;
+            Alunos = new List<Aluno>();
+        }
+
+        public int Id { get; private set; }
+        public int Id { get; private set; }
+        public List<Aluno> Alunos { get; private set; }
+    }
+}
